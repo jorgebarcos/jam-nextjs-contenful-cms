a93ab0425c235e119bcb18bed27503608db127a6
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-29 16:57:05 -0300

    isr

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ export async function getStaticProps() {
   return {
     props: {
       recipes: res.items
-    }
+    },
+    revalidate: 1
   }
 }
 
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -31,9 +31,8 @@ export async function getStaticProps({ params }) {
   })
 
   return {
-    props: {
-      recipe: items[0]
-    }
+    props: { recipe: items[0] },
+    revalidate: 1
   }
 }
 
